4005a4ad6106693e1de6c7ce15a2a7e4bfe7738d
Denys Nazarenko <denysnazarenko23@gmail.com>
2022-04-20 23:03:35 +0300

    small fix

--- src/components/audioPlayer/audioPlayer.js
+++ src/components/audioPlayer/audioPlayer.js
@@ -1,89 +1,89 @@
-import { useState, useEffect } from "react";
-import s from "./audioPlayer.module.css";
+import { useState, useEffect } from 'react';
+import s from './audioPlayer.module.css';
 
 const mockRecords = [
   {
     id: 1,
-    name: "Probe Title XYZ",
+    name: 'Dubstep',
     year: 2020,
-    src: "https://www.bensound.com/bensound-music/bensound-dubstep.mp3",
+    src: 'https://www.bensound.com/bensound-music/bensound-dubstep.mp3',
   },
   {
     id: 2,
-    name: "Probe Title ZXY",
+    name: 'Sunny',
     year: 2021,
-    src: "https://www.bensound.com/bensound-music/bensound-sunny.mp3",
+    src: 'https://www.bensound.com/bensound-music/bensound-sunny.mp3',
   },
   {
     id: 3,
-    name: "Probe Title YZX",
+    name: 'Tenderness',
     year: 2019,
-    src: "https://www.bensound.com/bensound-music/bensound-tenderness.mp3",
+    src: 'https://www.bensound.com/bensound-music/bensound-tenderness.mp3',
   },
   {
     id: 4,
-    name: "Probe Title XYZ",
+    name: 'Funny song',
     year: 2020,
-    src: "https://www.bensound.com/bensound-music/bensound-funnysong.mp3",
+    src: 'https://www.bensound.com/bensound-music/bensound-funnysong.mp3',
   },
   {
     id: 5,
-    name: "Probe Title ZXY",
+    name: 'Once again',
     year: 2021,
-    src: "https://www.bensound.com/bensound-music/bensound-onceagain.mp3",
+    src: 'https://www.bensound.com/bensound-music/bensound-onceagain.mp3',
   },
   {
     id: 6,
-    name: "Probe Title YZX",
+    name: 'Epic',
     year: 2019,
-    src: "https://www.bensound.com/bensound-music/bensound-epic.mp3",
+    src: 'https://www.bensound.com/bensound-music/bensound-epic.mp3',
   },
 ];
-const categories = ["All", "Audiobook", "Voiceover", "Games"];
+const categories = ['All', 'Audiobook', 'Voiceover', 'Games'];
 
 export default function AudioPlayer() {
   const [recordings, setRecordings] = useState(
-    () => JSON.parse(localStorage.getItem("recordings")) ?? mockRecords
+    () => JSON.parse(localStorage.getItem('recordings')) ?? mockRecords,
   );
 
   useEffect(() => {
-    localStorage.setItem("recordings", JSON.stringify(recordings));
+    localStorage.setItem('recordings', JSON.stringify(recordings));
   }, [recordings]);
 
-  const onClick = (e) => {
+  const onClick = e => {
     const { classList } = e.target;
 
-    if (classList.contains("fa-play") && e.target !== e.currentTarget) {
-      classList.add("fa-pause");
-      classList.remove("fa-play");
+    if (classList.contains('fa-play') && e.target !== e.currentTarget) {
+      classList.add('fa-pause');
+      classList.remove('fa-play');
       e.currentTarget.firstChild.play();
-    } else if (!classList.contains("fa-play") && e.target !== e.currentTarget) {
-      classList.remove("fa-pause");
-      classList.add("fa-play");
+    } else if (!classList.contains('fa-play') && e.target !== e.currentTarget) {
+      classList.remove('fa-pause');
+      classList.add('fa-play');
       e.currentTarget.firstChild.pause();
     }
   };
 
-  const changeName = (e) => {
+  const changeName = e => {
     setRecordings(() => {
       for (const record of recordings) {
         if (record.id === Number(e.target.dataset.nameid)) {
           record.name = e.target.value.trim();
-          localStorage.setItem("recordings", JSON.stringify(recordings));
+          localStorage.setItem('recordings', JSON.stringify(recordings));
           return recordings;
         }
       }
     });
   };
 
-  const changeYear = (e) => {
+  const changeYear = e => {
     setRecordings(() => {
       for (const record of recordings) {
         if (record.id === Number(e.target.dataset.yearid)) {
           const isYear = e.target.value.trim().match(/\d{4}/);
-          record.year = isYear ? Number(isYear[0]) : " ";
-          e.target.value = isYear ? `[${isYear[0]}]` : "[ ]";
-          localStorage.setItem("recordings", JSON.stringify(recordings));
+          record.year = isYear ? Number(isYear[0]) : ' ';
+          e.target.value = isYear ? `[${isYear[0]}]` : '[ ]';
+          localStorage.setItem('recordings', JSON.stringify(recordings));
           return recordings;
         }
       }
@@ -93,7 +93,7 @@ export default function AudioPlayer() {
   return (
     <div className={s.playerContainer}>
       <ul className={s.categoriesList}>
-        {categories.map((category) => {
+        {categories.map(category => {
           return (
             <li
               key={categories.indexOf(category)}
@@ -107,7 +107,7 @@ export default function AudioPlayer() {
         })}
       </ul>
       <ul className={s.recordsList}>
-        {recordings.map((record) => {
+        {recordings.map(record => {
           return (
             <li key={record.id} id={record.id} className={s.recordsListRecord}>
               <div className={s.recordButtons}>
